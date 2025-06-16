7bd1f1d9a9b97ed074d21ad30746841599e11366
Chamod-Dilhara <vishwachamoddil@gmail.com>
2025-06-16 10:19:21 +0530

    Initial commit: Caesar cipher Web App

---
+++ script.js
@@ -0,0 +1,33 @@
+function caesarCipher(str, key, decrypt = false) {
+  if (decrypt) key = (26 - key) % 26;
+  return str
+    .split('')
+    .map(char => {
+      if (char.match(/[a-z]/i)) {
+        const code = char.charCodeAt(0);
+        const base = code >= 97 ? 97 : 65;
+        return String.fromCharCode(((code - base + key) % 26) + base);
+      }
+      return char;
+    })
+    .join('');
+}
+
+function encrypt() {
+  const message = document.getElementById('message').value;
+  const key = parseInt(document.getElementById('key').value);
+  document.getElementById('output').value = caesarCipher(message, key);
+}
+
+function decrypt() {
+  const message = document.getElementById('message').value;
+  const key = parseInt(document.getElementById('key').value);
+  document.getElementById('output').value = caesarCipher(message, key, true);
+}
+
+function copyText() {
+  const output = document.getElementById('output');
+  output.select();
+  document.execCommand('copy');
+  alert('Text copied to clipboard!');
+}
